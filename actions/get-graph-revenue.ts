259c8d15e15bc69561a0420752923dca00034c83
Edita <edita.henriquezv@gmail.com>
2024-01-31 16:16:58 -0500

    First commit

---
+++ actions/get-graph-revenue.ts
@@ -0,0 +1,57 @@
+import prismadb from "@/lib/prismadb";
+
+interface GraphData {
+    name: string;
+    total: number;
+}
+
+export const getGraphRevenue = async (storeId: string): Promise<GraphData[]> => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+        storeId,
+        isPaid: true
+    },
+    include: {
+        orderItems:{
+            include: {
+                product: true
+            }
+        }
+    }
+  });
+
+  const montlyRevenue: {[key: number]: number} = {};
+  
+  //Grouping the orders by month and summing the revenue
+  for (const order of paidOrders) {
+    const month = order.createdAt.getMonth();
+    let revenueForOrder = 0;
+    for (const item of order.orderItems){
+      revenueForOrder += item.product.price.toNumber();
+    }
+    //Adding the revenue for this order to the respective month
+    montlyRevenue[month] = (montlyRevenue[month] || 0) + revenueForOrder;
+  }
+
+  //Converting the grouped data into the format expected by the graph
+  const graphData: GraphData[] = [
+    {name: 'Jan', total: 0},
+    {name: 'Feb', total: 0},
+    {name: 'Mar', total: 0},
+    {name: 'Apr', total: 0},
+    {name: 'May', total: 0},
+    {name: 'Jun', total: 0},
+    {name: 'Jul', total: 0},
+    {name: 'Aug', total: 0},
+    {name: 'Sep', total: 0},
+    {name: 'Oct', total: 0},
+    {name: 'Nov', total: 0},
+    {name: 'Dec', total: 0},
+  ];
+
+  //Filling in the revenue data
+  for (const month in montlyRevenue) {
+    graphData[parseInt(month)].total = montlyRevenue[parseInt(month)];
+  }
+  return graphData;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import { ModalProvider } from '@/providers/modal-provider'
+import './globals.css'
+import { ToastProvider } from '@/providers/toast-provide'
+import { ThemeProvider } from '@/providers/theme-provider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'E_Commerce Dashboard',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+          </body>
+      </html>
+    </ClerkProvider>
+  )
+}
